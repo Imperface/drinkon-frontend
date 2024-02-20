3c65168517c932c42e3ca2c91d8b197d12c76cc1
Imperface <130443062+Imperface@users.noreply.github.com>
2024-02-20 00:49:27 +0000

    Deploying to gh-pages from @ Imperface/drinkon-frontend@5b6a134337553cd0e7f87f6d8e35d5fe3bb7d10e 🚀

---
+++ assets/AddDrink-f6a8ad7f.js
@@ -0,0 +1,1 @@
+import{s as r,j as s}from"./index-8ba87c20.js";const d=r.div``,n=()=>s.jsx(d,{children:"AddDrink"});export{n as default};
---
+++ assets/Favorites-9cfcf199.js
@@ -0,0 +1,1 @@
+import{s,j as r}from"./index-8ba87c20.js";const t=s.div``,o=()=>r.jsx(t,{children:"Favorites"});export{o as default};
