6c5a25754ba94a59c7032c179d4039fd206d73f6
Nataliya-Viktorivna-Pashchenko <130203943+Nataliya-Viktorivna-Pashchenko@users.noreply.github.com>
2024-02-20 17:39:35 +0100

    change_2

--- src/components/pageTitle/PageTitle.jsx
+++ src/components/pageTitle/PageTitle.jsx
@@ -1,10 +1,4 @@
 import { Title } from "./PageTitle.styled"
 
-export const PageTitle = (title) => {
-    return (
-        
-            <Title>{title.name}</Title>
-        
-    )
-}
-// Для використання в інших компонентах імпортувати та використати там де треба : <Title name = 'My drinks'/>
+export const PageTitle = ({name}) => {return name && <Title>{name}</Title>}
+
