afbe62607a43f345cbfa1dbce0ef5ce595ddff21
Imperface <kovzhenko.ua@gmail.com>
2024-02-21 01:25:51 +0200

    fix routes

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,6 +1,6 @@
-export const HOME_ROUTE = '/';
+export const WELCOME_ROUTE = '/';
+export const HOME_ROUTE = '/home';
 export const DRINKS_ROUTE = '/drinks';
 export const ADD_DRINK_ROUTE = '/add-drink';
 export const MY_DRINKS_ROUTE = '/my-drinks';
 export const FAVORITES_ROUTE = '/favorites';
-export const WELCOME_ROUTE = '/welcome';
