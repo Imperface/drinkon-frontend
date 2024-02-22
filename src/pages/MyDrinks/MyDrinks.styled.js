ca9842def5bbb1f1412527d3336221d7c323ee7b
Chystiakov73Viktor <130795120+Chystiakov73Viktor@users.noreply.github.com>
2024-02-22 12:50:55 +0200

    add style MyDrinks

--- src/pages/MyDrinks/MyDrinks.styled.js
+++ src/pages/MyDrinks/MyDrinks.styled.js
@@ -1,3 +1,22 @@
 import { styled } from 'styled-components';
+import { device } from 'constants/device';
 
-export const MyDrinksWrapper = styled.div``;
+export const MyDrinksWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  & .myDrinks {
+    padding-top: 80px;
+    padding-bottom: 80px;
+  }
+
+  @media ${device.tablet} {
+    padding-top: 140px;
+    padding-bottom: 140px;
+  }
+
+  @media ${device.desktop} {
+    padding-top: 158px;
+    padding-bottom: 140px;
+  }
+`;
