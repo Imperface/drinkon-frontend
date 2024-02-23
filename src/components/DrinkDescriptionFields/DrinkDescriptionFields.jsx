30412c2140463f4e0449267349d1a038b1444e37
Anton Vasyliev <anton12vasyliev@gmail.com>
2024-02-23 18:03:15 +0000

    feature/drink-description

--- src/components/DrinkDescriptionFields/DrinkDescriptionFields.jsx
+++ src/components/DrinkDescriptionFields/DrinkDescriptionFields.jsx
@@ -1,8 +1,36 @@
-import { DrinkDescriptionFieldsWrapper } from './DrinkDescriptionFields.styled'
+import { DrinkDescriptionFieldsWrapper,AddDescriptionForm,StyledDropzone,DropzoneText,AddDescriptionFormWrapper } from './DrinkDescriptionFields.styled'
+import {useDropzone} from 'react-dropzone';
+import { IoIosAdd } from "react-icons/io";
 
 export const DrinkDescriptionFields = () => {
-return (
-   <DrinkDescriptionFieldsWrapper>DrinkDescriptionFieldsWrapper</DrinkDescriptionFieldsWrapper>
-   
-)
+   const {
+   getRootProps,
+   getInputProps,
+   isFocused,
+   isDragAccept,
+   isDragReject
+   } = useDropzone({accept: {'image/*': []}});
+
+   return (
+   <DrinkDescriptionFieldsWrapper>
+      <StyledDropzone {...getRootProps({isFocused, isDragAccept, isDragReject})}>
+         <input {...getInputProps()} />
+            <IoIosAdd style={{
+                        width: '50px',
+                        height: '50px',
+                        color: '#161F37', 
+                        fontSize: '48px', 
+                        background: '#F3F3F3',
+                        borderRadius: '10px', 
+            }}/>
+            <DropzoneText>Add image</DropzoneText>
+      </StyledDropzone>
+      <AddDescriptionFormWrapper>
+      <AddDescriptionForm>
+         <p>DescriptionForm</p>
+      </AddDescriptionForm>
+      </AddDescriptionFormWrapper>
+   </DrinkDescriptionFieldsWrapper>
+   );
 };
+
--- src/components/DrinkDescriptionFields/DrinkDescriptionFields.styled.js
+++ src/components/DrinkDescriptionFields/DrinkDescriptionFields.styled.js
@@ -1,5 +1,41 @@
 import {styled} from 'styled-components'
 
 export const DrinkDescriptionFieldsWrapper = styled.div`
+display: flex;
+margin-bottom: 80px;
+/* margin-right: 90px; */
+gap: 40px;
+`;
 
+export const StyledDropzone = styled.div`
+   width: 400px;
+   height: 400px;
+   flex: 1;
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   justify-content: center;
+   padding: 20px;
+   border-width: 2px;
+   border-radius: 10px;
+   border-color: #000000;
+   background: #161F37;
+   outline: black;
+`;
+export const DropzoneText = styled.p`
+color: #F3F3F3;
+margin-top: 18px;
+`;
+export const AddDescriptionFormWrapper = styled.div`
+width: 393px;
+height: 320px;
+border: 2px;
+color: white;
+display: flex;
+flex-direction: column;
+`;
+export const AddDescriptionForm = styled.form`
+gap: 20px;
+display: flex;
+flex-direction: column;
 `;
