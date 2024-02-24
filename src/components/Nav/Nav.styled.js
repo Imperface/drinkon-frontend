85c6b2e214163e632ff253ae176a300c42c9f5ff
Vitalii Kyi <kyi.profy@gmail.com>
2024-02-24 16:37:15 +0200

    Update Nav.styled.js

--- src/components/Nav/Nav.styled.js
+++ src/components/Nav/Nav.styled.js
@@ -8,7 +8,7 @@ export const NavList = styled.ul`
   flex-direction: column;
   gap: 14px;
 
- @media screen and (min-width: 768px) {
+  @media screen and (min-width: 768px) {
     gap: 16px;
     margin-top: 40px;
   }
@@ -25,10 +25,13 @@ export const NavItem = styled.li`
   &:hover,
   &:focus {
     transform: scale(1.1);
-    text-shadow: 1px 1px 2px black, 0 0 25px blue, 0 0 5px darkblue;
+    text-shadow:
+      1px 1px 2px black,
+      0 0 25px blue,
+      0 0 5px darkblue;
   }
 `;
 
 export const NavLink = styled(Link)`
-  color: ${(props) => props.theme.whiteColor}; 
+  color: ${(props) => props.theme.whiteColor};
 `;
