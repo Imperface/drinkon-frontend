d6df9c6f33af0a1b73e174256c478f159e9d1b6b
Volodya66 <130245015+Volodya66@users.noreply.github.com>
2024-02-24 13:45:35 +0200

    Update LogoutButton.jsx

--- src/components/LogoutButton/LogoutButton.jsx
+++ src/components/LogoutButton/LogoutButton.jsx
@@ -2,14 +2,21 @@ import { ButtonWrapper } from './LogoutButton.styled';
 
 import { useDispatch } from 'react-redux';
 import { signoutThunk } from '../../redux/users/operations';
+import { selectUserData } from '../../redux/users/selectors';
 import { Notify } from 'notiflix';
+
+import { useSelector } from 'react-redux';
 
 const LogoutButton = ({ classNameButton, closingSignal }) => {
   const dispatch = useDispatch();
+  const data = useSelector(selectUserData);
 
   const onClick = async () => {
     try {
-      const { error, payload } = await dispatch(signoutThunk());
+      const { error, payload } = await dispatch(
+        signoutThunk({ _id: data._id })
+      );
+      console.log('data._id: ', data._id);
       if (error) {
         Notify.failure(payload);
         return;
