576d48d01906271ee139dda6a8dbadeaa2075761
Imperface <kovzhenko.ua@gmail.com>
2024-02-26 18:40:10 +0200

    fix styles privacy, termOfService

--- src/pages/Privacy/Privacy.styled.js
+++ src/pages/Privacy/Privacy.styled.js
@@ -1,7 +1,18 @@
 import { styled } from 'styled-components';
+import { device } from 'constants/device';
 
 export const PrivacyWrapper = styled.div`
   width: 100%;
+  font-size: 14px;
+  padding: 80px 0;
+
+  @media ${device.tablet} {
+    font-size: 18px;
+    padding: 120px 0 140px;
+  }
+  @media ${device.desktop} {
+    padding: 158px 0 140px;
+  }
   & h1 {
     margin-bottom: 30px;
   }
